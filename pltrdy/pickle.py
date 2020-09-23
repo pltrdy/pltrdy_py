421ed70bcb017bfa95e94b1628dcf93115145569
pltrdy <pltrdy@gmail.com>
2020-09-23 17:43:04 +0200

    pickle: load/save aliases

--- pltrdy/pickle.py
+++ pltrdy/pickle.py
@@ -1,6 +1,12 @@
 import pickle
 
 
+def load(*args, **kwargs):
+    return pickle_load(*args, **kwargs)
+
+def save(*args, **kwargs):
+    return pickle_save(*args, **kwargs)
+
 def pickle_load(path):
     with open(path, 'rb') as f:
         return pickle.load(f)
