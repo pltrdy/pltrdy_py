f86db37a8fa8fb8274c260757f8efe52a684668f
pltrdy <pltrdy@gmail.com>
2021-08-31 11:28:26 +0200

    0.2.2: add Tee class (like Unix's tee)

--- pltrdy/__init__.py
+++ pltrdy/__init__.py
@@ -6,9 +6,10 @@ import pltrdy.pickle
 import pltrdy.rouge
 import pltrdy.stats
 import pltrdy.time
+import pltrdy.tee
 import pltrdy.wc  # noqa F401
 
 from pltrdy.describe import describe_str, describe_lines, describe, tab_print
 from pltrdy.reload import reload_class
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
