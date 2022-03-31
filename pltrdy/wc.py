6cca8d1d69f43a7a811591515ea33888f59bed35
pltrdy <pltrdy@gmail.com>
2022-03-31 18:06:22 +0200

    0.3.7: fix linecount

--- pltrdy/wc.py
+++ pltrdy/wc.py
@@ -6,10 +6,13 @@ import subprocess
 
 
 def linecount(path):
-    return wordcount(path, args="-l")
+    return wordcount(path, args=["-l"])
 
 
 def wordcount(path, args=["-w"]):
+    if isinstance(args, str):
+        args = args.split()
+
     o = subprocess.run(["wc"] + args + [path], stdout=subprocess.PIPE)
     wc = int(o.stdout.decode('utf-8').split()[0])
     return wc
