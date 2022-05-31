eee9d7a37427799d2365218afe84522da4b66685
pltrdy <pltrdy@gmail.com>
2022-05-31 11:47:30 +0200

    0.3.8: git utility to get revision hash

--- pltrdy/__init__.py
+++ pltrdy/__init__.py
@@ -12,6 +12,7 @@ import pltrdy.quiet
 import pltrdy.tmpdir
 import pltrdy.types
 import pltrdy.cmd
+import pltrdy.git
 
 from pltrdy.describe import describe_str, describe_lines, describe, tab_print
 from pltrdy.reload import reload_class
