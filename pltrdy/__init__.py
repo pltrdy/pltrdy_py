e41d820fc181fb5ddeb801ce1787f540b57ca3e1
pltrdy <pltrdy@gmail.com>
2022-05-31 11:53:21 +0200

    0.3.9: fix code mismatch of 0.3.8 w/ rebase master

--- pltrdy/__init__.py
+++ pltrdy/__init__.py
@@ -17,4 +17,4 @@ import pltrdy.git
 from pltrdy.describe import describe_str, describe_lines, describe, tab_print
 from pltrdy.reload import reload_class
 
-__version__ = "0.3.8"
+__version__ = "0.3.9"
